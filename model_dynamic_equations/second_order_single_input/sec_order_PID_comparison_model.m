427002cb7e8cc10c394e5a8232646036644d26d6
Andy Zelenak <andyz@utexas.edu>
2015-09-05 13:36:25 -0600

    --

---
+++ model_dynamic_equations/second_order_single_input/sec_order_PID_comparison_model.m
@@ -0,0 +1,26 @@
+function dx = sec_order_PID_comparison_model(t,x,u)
+
+
+global num_inputs num_states
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dx: Returns the time derivatives of the state variables, x-dot.
+
+% Do not modify anything in this top section.
+
+% Needs to return a (num_states + num_inputs) vector since the input is
+% (num_states + num_inputs)-long.
+% The final entries in the returned vector are meaningless.
+
+% Pad the end of the vector to the right size.
+for i=num_states+1 : num_states+num_inputs
+    dx(i) = 0;
+end
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Define the system here:
+
+dx(1) = 1000000*x(2);
+dx(2) = (1/2)*(-4*x(2)-.000003*x(1)+u);
