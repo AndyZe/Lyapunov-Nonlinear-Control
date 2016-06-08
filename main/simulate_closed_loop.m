0d128c9075aeea411b82c1508b23770051934fab
Andy Zelenak <andyz@utexas.edu>
2016-06-08 17:53:04 -0500

    First commit for this double integrator.

--- main/simulate_closed_loop.m
+++ main/simulate_closed_loop.m
@@ -47,20 +47,20 @@ else % We're past the first epoch, go normally
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
     % TODO: an extensible method of making these calculations
-    dh_dx = [0 1 0]; % 1x3
+    dh_dx = [1 0 0]; % 1x3
     
-    f = [ -x(epoch,1);
+    f = [ x(epoch,2)+x(epoch,3);
         x(epoch,3);
-        x(epoch,1)*x(epoch,3) ];
+        0 ];
     % 3x1
     
     Lf_h = dh_dx * f; % scalar
     
-    dLf_h_dx = [0 0 1]; % 1x3
+    dLf_h_dx = [x(epoch,2)+x(epoch,3) 0 0]; % 1x3
     
     Lf_2_h = dLf_h_dx*f; % scalar
     
-    g = [(2+x(epoch,3)^2)/(1+x(epoch,3)^2); 0; 1]; % 3x1
+    g = [0; 0; 1]; % 3x1
     
     Lg_Lf_h = dLf_h_dx * g; % scalar, dLf_h/dx*g
     
@@ -166,7 +166,7 @@ epoch=epoch+1; % Count another epoch
 
 t(epoch) = time(end); % Update time for the next epoch
 
-y(epoch) = x_traj(end,2);
+y(epoch) = x_traj(end,1); % y=x1
 V(epoch)= 0.5*dot(xi,xi);
 
 for i=1 : num_states
